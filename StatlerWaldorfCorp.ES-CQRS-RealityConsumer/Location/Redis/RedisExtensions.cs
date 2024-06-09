6163b2e1cc0c95b6942aaa3346172e24afd49e07
Erbynn John <john.erbynn@gmail.com>
2024-06-09 09:10:30 -0500

    add service connection extension for redis

--- StatlerWaldorfCorp.ES-CQRS-RealityConsumer/Location/Redis/RedisExtensions.cs
+++ StatlerWaldorfCorp.ES-CQRS-RealityConsumer/Location/Redis/RedisExtensions.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace StatlerWaldorfCorp.ES_CQRS_RealityConsumer.Location.Redis
 {
-    public class RedisExtensions
+    public static class RedisExtensions
     {
-        
+        public static IServiceCollection AddRedisConnectionMultiplexer(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null){
+                throw new ArgumentNullException(nameof(services));
+            }
+
+             if(configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            var redisConfig = configuration.GetSection("redis:configstring").Value;
+
+            services.AddSingleton(typeof(IConnectionMultiplexer), ConnectionMultiplexer.ConnectAsync(redisConfig).Result);
+
+            return services;
+        }
     }
 }
